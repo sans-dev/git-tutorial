aaa3930cf4673ea7ea929097a26a515987c4b148
Sebastian Sander <46248180+sans-dev@users.noreply.github.com>
2022-10-21 08:22:10 +0200

    Update main.R

--- main.R
+++ main.R
@@ -2,7 +2,7 @@ source("functions/fahrenheit_to_celsius.R")
 source("functions/celsius_to_kelvin.R")
 source("functions/farenheit_to_kelvin.R")
 
-temp_F = 70
+temp_F = 78
 
 temp_C = fahrenheit_to_celsius(temp_F)
 temp_K = celsius_to_kelvin(temp_C)
